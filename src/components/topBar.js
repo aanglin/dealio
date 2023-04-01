dbfe88d291e5cc2bdd50987e16337917651a5673
Aaron Anglin <aaron.anglin101@gmail.com>
2023-04-01 17:43:52 -0500

    made topbar responsive

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,28 +1,37 @@
 import Link from "next/link";
-import Nav from "./nav"
+import React, { useContext } from 'react'
 import { Dancing_Script} from 'next/font/google'
+import { AiOutlineShoppingCart, AiOutlineHome } from "react-icons/ai";
+import { Context } from "./context"
 
 
 const design = Dancing_Script({ weight: '700', subsets: ['latin'] })
 
 function TopBar() {
- 
-
+  const {selectedProducts} = useContext(Context)
   return (
     <div className="pb-28">
-    <div className="fixed flex justify-evenly items-center text-xl  pl-4 bg-[#64D4D9] w-full h-[5rem] z-50">
+    <div className="fixed flex justify-start items-center text-xl  pl-4 bg-[#64D4D9] w-full h-[5rem] z-50">
       <div className="flex items-center">
-        <div className="flex  text-5xl text-[#195759]">
+        <div className="flex text-3xl text-[#195759] sm:text-5xl">
         <h1 className={design.className}>Dealio</h1>
         </div>
-        <ul className='flex pl-2'>
+        <ul className='pl-2 hidden lg:flex'>
         <li><Link href={'#jewelery'} className="inline-block">Jewelery</Link></li>
         <li> <Link href={'#electronics'}className="inline-block">Electronics</Link></li>
         <li> <Link href={'#ladies'}className="inline-block">Womens</Link></li>
         <li><Link href={'#guys'}className="inline-block">Mens</Link></li>
         </ul>
-        <Nav />
-      
+        <div className='flex'>
+        <Link href={'/'} className="flex px-11 items-center sm:px-24">
+        <AiOutlineHome size={30} className="text-[#195759]"/>
+        <h1 className="cursor-pointer text-[#195759]">Home</h1>
+        </Link>
+        <Link href={'/checkout'} className="flex justify-center items-center">
+        <AiOutlineShoppingCart size={30} className="flex items-center text-[#195759]"/>
+        <h1 className="px-3 cursor-pointer text-[#195759]">Cart <span className="text-white">{selectedProducts.length}</span></h1>
+        </Link>
+    </div>
       </div>
     </div>
     </div>
