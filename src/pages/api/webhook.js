6cda21e2ecd6c5f1e62ce1125d7b86e565047580
Aaron Anglin <aaron.anglin101@gmail.com>
2023-04-01 11:15:18 -0500

    added webhooks for stripe

---
+++ src/pages/api/webhook.js
@@ -0,0 +1,29 @@
+import { db } from '../../lib/connect';
+import { buffer } from 'micro'
+import  Order from '../../models/order'
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+async function handler(req,res) {
+    await db;
+    const signingSecret = process.env.Signing_SECRET
+    const payload = await buffer(req);
+    const signature = req.headers['stripe-signature'];
+    const event = stripe.webhooks.constructEvent(payload, signature, signingSecret);
+
+    if (event?.type === 'checkout.session.completed') {
+      const metadata = event.data?.object?.metadata;
+      const paymentStatus = event.data?.object?.payment_status;
+      if (metadata?.orderId && paymentStatus === 'paid') {
+       await Order.findByIdAndUpdate(metadata.orderId,{paid:1});
+      }  
+    }
+
+    res.json('ok')
+}
+export default handler;
+
+export const config = {
+    api: {
+        bodyParser: false, 
+    }
+}
