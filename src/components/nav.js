9b65fbb10c1b51cbccdf205cfdae1aff7cae6645
Aaron Anglin <aaron.anglin101@gmail.com>
2023-04-07 11:58:48 -0500

    checkout responsive fixing

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,14 +6,14 @@ import { Context } from "./context"
 function Nav() {
     const {selectedProducts} = useContext(Context)
   return (
-    <div className='flex'>
-        <Link href={'/'} className="flex items-center px-24">
-        <AiOutlineHome size={30} className="text-[#195759]"/>
-        <h1 className="px-3 cursor-pointer text-[#195759]">Home</h1>
+    <div className='flex px-32'>
+        <Link href={'/'} className="flex justify-center items-center ">
+        <AiOutlineHome size={30} className="inline-block text-[#195759]"/>
+        <h1 className="inline-block pl-3  cursor-pointer text-[#195759]">Home</h1>
         </Link>
-        <Link href={'/checkout'} className="flex justify-center items-center">
-        <AiOutlineShoppingCart size={30} className="flex items-center text-[#195759]"/>
-        <h1 className="px-3 cursor-pointer text-[#195759]">Cart <span className="text-white">{selectedProducts.length}</span></h1>
+        <Link href={'/checkout'} className="flex pl-44 justify-center items-center">
+        <AiOutlineShoppingCart size={30} className="inline-block items-center text-[#195759]"/>
+        <h1 className="pl-3 cursor-pointer text-[#195759]">Cart <span className="text-white">{selectedProducts.length}</span></h1>
         </Link>
     </div>
   )
