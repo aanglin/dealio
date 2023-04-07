0f1a58d2c1c5d86677a299edf89826ba5e2291ba
Aaron Anglin <aaron.anglin101@gmail.com>
2023-04-07 15:06:54 -0500

    fixed responsiveness on mobile devices

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -79,8 +79,8 @@ const total = (Number(subTotal) + parseFloat(deliveryPrice)).toFixed(2); // calc
         <div className='p-3 shrink-0 w-24'>
           <Image src={productInfo.image} width={1920} height={1080} alt="/" />
         </div>
-        <div className='pl-1'>
-          <h3 className='font-bold text-lg'>{productInfo.title}</h3>
+        <div>
+          <h3 className='font-bold text-lg pr-5 p-1 m-1'>{productInfo.title}</h3>
           <p className='w-[20rem] pr-7 md:w-[45rem] text-sm'>{productInfo.description}</p>
           <div className='flex'>
             <div className='grow font-bold'>
@@ -99,10 +99,10 @@ const total = (Number(subTotal) + parseFloat(deliveryPrice)).toFixed(2); // calc
     ))}
     <form action="/api/checkout" method='POST' className='md:pl-16'>
         <div className='flex flex-col p-4 w-[27rem] md:w-[82%]'>
-          <input name="name" value={name} onChange={e => setName(e.target.value) } className='bg-gray-100 w-full md:w-1/2 rounded-lg px-4 py-2 mb-3' type="text" placeholder='Name' />
-          <input name="address" value={address} onChange={e => setAddress(e.target.value) } className='bg-gray-100 w-full md:w-1/2 rounded-lg px-4 py-2 mb-3' type="text" placeholder='Street Address' />
-          <input name="city" value={city} onChange={e => setCity(e.target.value) } className='bg-gray-100 w-full md:w-1/2 rounded-lg px-4 py-2 mb-3' type="text" placeholder='City and Zip Code' />
-          <input name="email" value={email} onChange={e => setEmail(e.target.value) } className='bg-gray-100 w-full md:w-1/2 rounded-lg px-4 py-2 mb-3' type="email" placeholder='Email' />
+          <input name="name" value={name} onChange={e => setName(e.target.value) } className='bg-gray-100 w-[92%] md:w-1/2 rounded-lg px-4 py-2 mb-3' type="text" placeholder='Name' />
+          <input name="address" value={address} onChange={e => setAddress(e.target.value) } className='bg-gray-100 w-[92%] md:w-1/2 rounded-lg px-4 py-2 mb-3' type="text" placeholder='Street Address' />
+          <input name="city" value={city} onChange={e => setCity(e.target.value) } className='bg-gray-100 w-[92%] md:w-1/2 rounded-lg px-4 py-2 mb-3' type="text" placeholder='City and Zip Code' />
+          <input name="email" value={email} onChange={e => setEmail(e.target.value) } className='bg-gray-100 w-[92%] md:w-1/2 rounded-lg px-4 py-2 mb-3' type="email" placeholder='Email' />
         </div>
         <div className='mt-4 pl-5  w-[31rem] md:w-[82%]'>
           <div className='flex my-3 w-1/2'>
@@ -118,7 +118,7 @@ const total = (Number(subTotal) + parseFloat(deliveryPrice)).toFixed(2); // calc
             <h3 className='font-bold'>${total}</h3>
           </div>
             <input type="hidden" name='products' value={selectedProducts.join(',')} />
-        <button type='submit' className='bg-emerald-500  py-2 rounded-lg text-white w-[25rem] md:w-1/2 my-4 shadow-lg hover:shadow-emerald-100'>Pay ${total}</button>
+        <button type='submit' className='bg-emerald-500  py-2 rounded-lg text-white w-[22rem] md:w-1/2 my-4 shadow-lg hover:shadow-emerald-100'>Pay ${total}</button>
           </div>
         </form>
         </div>
