c870083e68b59b072e65d02c21c4d0a7ec028ac8
Aaron Anglin <aaron.anglin101@gmail.com>
2023-04-08 15:05:32 -0500

    adjusted topbar width

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -11,19 +11,19 @@ function TopBar() {
   const {selectedProducts} = useContext(Context)
   return (
     <div className="pb-28">
-    <div className="fixed flex justify-start items-center text-xl  pl-4 bg-[#64D4D9] w-full h-[5rem] z-50">
+    <div className="fixed flex justify-start items-center text-xl  pl-3 bg-[#64D4D9] w-full h-[5rem] z-50">
       <div className="flex items-center">
         <div className="flex text-3xl text-[#195759] md:pl-3 sm:text-5xl">
         <h1 className={design.className}>Dealio</h1>
         </div>
-        <ul className='pl-2 hidden lg:flex'>
+        <ul className='pl-2 hidden lg:flex '>
         <li><Link href={'#jewelery'} className="inline-block">Jewelery</Link></li>
         <li> <Link href={'#electronics'}className="inline-block">Electronics</Link></li>
         <li> <Link href={'#ladies'}className="inline-block">Womens</Link></li>
         <li><Link href={'#guys'}className="inline-block">Mens</Link></li>
         </ul>
         <div className='flex'>
-        <Link href={'/'} className="flex px-11 items-center md:pl-[23rem] lg:p-36">
+        <Link href={'/'} className="flex px-11 items-center md:pl-[23rem] lg:p-36 lg:px-20">
         <AiOutlineHome size={30} className="text-[#195759]"/>
         <h1 className="cursor-pointer text-[#195759]">Home</h1>
         </Link>
